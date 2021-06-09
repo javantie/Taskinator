b48744f419103756a7c61597038d3083669c02b1
Akeem C <javantieclarke@gmail.com>
2021-06-09 08:08:47 -0400

    fixed default refresh issue on add task button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,21 @@
-var buttonEl = document.querySelector("#save-task"); 
+var formEl = document.querySelector("#task-form");
+
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+//var taskSumbit = document.querySelector(".text-input")
+
+
 //Take handler function who's only take is to handle the adds task function.
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
 }
-
-buttonEl.addEventListener ("click", createTaskHandler);
-
+//taskSumbit.addEventListener("clcik", createTaskHandler);
+formEl.addEventListener ("submit", createTaskHandler);
 
 
 
