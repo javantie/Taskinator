a2c58a93f7c0c7e60edc268a5bc5642a0f29022e
Akeem C <javantieclarke@gmail.com>
2021-06-13 17:23:31 -0400

    Updated Local Storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,6 +64,7 @@ var createTaskEl = function(taskDataObj) {
   taskDataObj.id = taskIdCounter;
 
   tasks.push(taskDataObj);
+  saveTasks();
 
   var taskActionsEl = createTaskActions(taskIdCounter);
   
@@ -152,6 +153,7 @@ var deleteTask = function(taskId) {
 
   // reassign tasks array to be the same as updatedTaskArr
   tasks = updatedTaskArr;
+  saveTasks();
 };
 
 var editTask = function(taskId) {
@@ -185,6 +187,7 @@ var completeEditTask = function(taskName, taskType, taskId) {
       tasks[i].type = taskType;
     }
   };
+  saveTasks();
 
   alert("Task Updated!");
   formEl.removeAttribute("data-task-id");
@@ -216,7 +219,12 @@ var taskStatusChangeHandler = function(event){
       tasks[i].status = statusValue;
     }
   }
+  saveTasks();
 };
+
+var saveTasks = function() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 //taskSumbit.addEventListener("clcik", createTaskHandler);
